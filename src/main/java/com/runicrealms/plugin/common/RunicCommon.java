2f2eacdbe61a7a2b592c1b08603a8b7f895099aa
Excel619 <excel8392@gmail.com>
2023-05-31 16:28:25 -0600

    Moved config API from core -> common

--- src/main/java/com/runicrealms/plugin/common/RunicCommon.java
+++ src/main/java/com/runicrealms/plugin/common/RunicCommon.java
@@ -1,21 +1,33 @@
 package com.runicrealms.plugin.common;
 
 import com.runicrealms.plugin.common.api.AchievementsAPI;
+import com.runicrealms.plugin.common.api.ConfigAPI;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class RunicCommon extends JavaPlugin {
 
     private static AchievementsAPI achievementsAPI;
+    private static ConfigAPI configAPI;
 
     public static AchievementsAPI getAchievementsAPI() {
         return achievementsAPI;
+    }
+
+    public static ConfigAPI getConfigAPI() {
+        return configAPI;
     }
 
     public static void registerAchievementsAPI(AchievementsAPI api) {
         if (achievementsAPI == null)
             throw new IllegalStateException("Achievements API can not be registered: already registered!");
         achievementsAPI = api;
+    }
+
+    public static void registerConfigAPI(ConfigAPI api) {
+        if (configAPI == null)
+            throw new IllegalStateException("Config API can not be registered: already registered!");
+        configAPI = api;
     }
 
     @Override
